29b31b6f6f7308787535c3e20446c2b07eab1868
lamine-br422 <laminebourou2005@gmail.com>
2025-10-27 23:15:45 +0100

    Update test_semester.py

--- tests/test_semester.py
+++ tests/test_semester.py
@@ -1,16 +1,18 @@
+import sys, os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
 from module import Module
 from unit import Unit
 from semester import Semester
 
-def test_semester_average_and_credit():
+def test_semester_average_and_credits():
     m1 = Module("MTI", "Methods", coef=3, credit=5)
     m1.set_grade(tp=10, exam=12)
     m2 = Module("AABD", "Database", coef=2, credit=4)
     m2.set_grade(tp=8, exam=6)
-    u = Unit("UEM11", "UE Méthodologie", [m1, m2])
-    s = Semester("S1", "Semester 1", [u])
+    u = Unit("UEM11", "Méthodologie", [m1, m2])
+    s = Semester("S1", "Semestre 1", [u])
     avg = s.calculate_average()
-    credit = s.calculate_credits()
+    credits = s.calculate_credits()
     assert isinstance(avg, float)
-    assert isinstance(credit, int)
-
+    assert isinstance(credits, int)
