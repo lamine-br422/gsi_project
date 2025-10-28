751956023eddc6723c6c8ac204a41c24f84356d5
lamine-br422 <laminebourou2005@gmail.com>
2025-10-28 08:33:51 +0100

    Update test_module.py

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,15 +1,14 @@
 import sys, os
-#sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append( "..")
 
-from .module import Module
+import module
 
 def test_module_average_and_credit():
-    m = Module("MTI", "Methods & Technologies of Implementation",
-               coef=3, credit=5, hours_tp=1.5,
-               continous_percent=40, exam_percent=60)
+    m = module.Module("MTI", "Methods & Technologies of Implementation",
+                      coef=3, credit=5, hours_tp=1.5,
+                      continous_percent=40, exam_percent=60)
     m.set_grade(tp=10, exam=12)
     avg = m.calculate_average()
     credit = m.calculate_credits()
     assert round(avg, 1) == 11.2
     assert credit == 5
-
